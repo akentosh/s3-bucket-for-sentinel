6a4f4d493ed4fd33346e70a95d22317448365842
akentosh <akentosh@gmail.com>
2019-05-30 11:30:45 -0400

    initial commit for s3 example with sentinel

---
+++ README.md
@@ -0,0 +1,6 @@
+# Example TF to create an S3 bucket
+
+This is an example to show Sentinel with Terraform in the most basic of sense. The important bits are that we stop the public bucket from ever being deployed. Lambda is great or cloud custodian however the resources get created before they are then shut down.
+
+
+Change acl from public-read to private to pass sentinel test.
---
+++ main.tf
@@ -0,0 +1,47 @@
+# S3 bucket config
+
+provider "tfe" {
+  hostname = var.tfe_hostname
+  token    = var.tfe_token
+  version  = ">= 0.4"
+}
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "akentosh"
+
+    workspaces {
+      name = "s3-bucket"
+    }
+  }
+}
+
+variable "tfe_token" {
+}
+
+variable "tfe_hostname" {
+  description = "The domain where your TFE is hosted."
+  default     = "app.terraform.io"
+}
+
+variable "tfe_organization" {
+  description = "The TFE organization to apply your changes to."
+  default     = "akentosh"
+}
+
+resource "aws_s3_bucket" "cool" {
+  bucket = "akentosh-super-cool-bucket"
+  acl    = "public-read"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+  tags = {
+    Owner   = "akentosh"
+  }
+}
