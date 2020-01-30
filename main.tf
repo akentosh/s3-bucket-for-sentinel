52a49eb9b4ded3a3f253a11c0a2d5cb5da47016f
Adam Kentosh <akentosh@gmail.com>
2020-01-29 19:02:13 -0500

    change to public

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "hugs-are-great"
-  acl    = "private"
+  acl    = "public"
 
   versioning = {
     enabled = true
